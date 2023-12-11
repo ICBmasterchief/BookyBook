1ce4325660d8f9de659766e3b74cbf1749f67d6e
ICBmasterchief <ignacioelincreible@hotmail.com>
2023-12-11 15:49:32 +0100

    User.cs modified

--- BookyBook.Domain/User.cs
+++ BookyBook.Domain/User.cs
@@ -4,9 +4,9 @@ public class User
     public string? Name { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
-    private int IdNumber { get; set; }
     public DateTime RegistrationDate { get; set; }
     public decimal PenaltyFee { get; set; }
+    private int IdNumber { get; set; }
     private static int IdNumberSeed = 1111;
 
     public User(){}
