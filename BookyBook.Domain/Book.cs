496c31f1025f6daba6f51e5812cd7ad8c1140928
ICBmasterchief <ignacioelincreible@hotmail.com>
2023-12-11 15:51:42 +0100

    Attributes and constructors added to Book class

--- BookyBook.Domain/Book.cs
+++ BookyBook.Domain/Book.cs
@@ -1,5 +1,24 @@
 namespace BookyBook.Domain;
 public class Book
 {
-    
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Genre { get; set; }
+    public int Year { get; set; }
+    public int Copies { get; set; } = 0;
+    public decimal Score { get; set; }
+    private int IdNumber { get; set; }
+    private static int IdNumberSeed = 10000;
+
+    public Book(){}
+    public Book(string title, string author, string genre, int year, decimal score){
+        this.Title = title;
+        this.Author = author;
+        this.Genre = genre;
+        this.Year = year;
+        this.Copies = this.Copies++;
+        this.Score = score;
+        this.IdNumber = IdNumberSeed;
+        IdNumberSeed++;
+    }
 }
