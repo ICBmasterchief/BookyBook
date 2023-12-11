c3e680a8d51760b9b37fee98a1ba246520bb6bee
ICBmasterchief <ignacioelincreible@hotmail.com>
2023-12-11 15:18:41 +0100

    added constructors to User class

--- BookyBook.Domain/User.cs
+++ BookyBook.Domain/User.cs
@@ -8,4 +8,15 @@ public class User
     public DateTime RegistrationDate { get; set; }
     public decimal PenaltyFee { get; set; }
     private static int IdNumberSeed = 1111;
+
+    public User(){}
+    public User(string name, string email, string password){
+        this.Name = name;
+        this.Email = email;
+        this.Password = password;
+        this.RegistrationDate = DateTime.Now;
+        this.PenaltyFee = 0;
+        this.IdNumber = IdNumberSeed;
+        IdNumberSeed++;
+    }
 }
