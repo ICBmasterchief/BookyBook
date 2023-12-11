bd4542e71393728890a0c39d100e2269418947af
ICBmasterchief <ignacioelincreible@hotmail.com>
2023-12-11 13:45:44 +0100

    First commit Develop

--- BookyBook.Domain/User.cs
+++ BookyBook.Domain/User.cs
@@ -1,5 +1,11 @@
 ﻿namespace BookyBook.Domain;
 public class User
 {
-
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    private int IdNumber { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public decimal PenaltyFee { get; set; }
+    private static int IdNumberSeed = 1111;
 }
